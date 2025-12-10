007aec39d5d9497d3c64713d32f6a62f4f8349ed
Maksim Buldau <maxbuldov@gmail.com>
2025-12-09 20:47:51 -0800

    config

--- lib/config.php
+++ lib/config.php
@@ -2,10 +2,18 @@
 require __DIR__ . '../vendor/autoload';
 
 \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET'));
-$allowed_origin = 'http://localhost:3000';
-// $allowed_origin = 'https://smartpeoplemoving.com';
+$allowed_origins = [
+    'http://localhost:3000',
+    'https://smartpeoplemoving.com',
+];
 
-$base_url     = 'https://db.smartpeoplemoving.com/wp-json';
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowed_origins, true)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+}
+
 $wp_login_url = $base_url . '/jwt-auth/v1/token';
 $wp_works_url = $base_url . '/wp/v2/works';
 
